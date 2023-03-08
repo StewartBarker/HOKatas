a15c4bfa0b0b35975968965c23b33b2f83eb73a3
Matt Coulton <mattcoulton@192.168.1.120>
2023-03-08 11:34:06 +0000

    Day 3 solution

---
+++ Advent3/Part1/Program.cs
@@ -0,0 +1,41 @@
+﻿var lines = File.ReadLines("input");
+
+int prioritySum = 0;
+foreach (String line in lines)
+{
+    int compartmentLength = line.Length / 2;
+
+    SortedSet<char> charSet = new SortedSet<char>();
+    for (int i = 0; i < compartmentLength; i++)
+    {
+        charSet.Add(line.ElementAt(i));
+    }
+    for (int i = compartmentLength; i < line.Length; i++)
+    {
+        if (charSet.Contains(line.ElementAt(i)))
+        {
+            prioritySum += GetItemPriority(line.ElementAt(i));
+            break;
+        }
+    }
+}
+
+Console.WriteLine("Sum of priorities: " + prioritySum);
+
+int GetItemPriority(char item)
+{
+    int priority;
+    int itemAscii = (int)item;
+    if (itemAscii is >= 65 and <= 90)
+    {
+        //Uppercase letter
+        priority = itemAscii - 38;
+        return priority;
+    } else if (itemAscii is >= 97 and <= 122)
+    {
+        //Lowercase letter
+        priority = itemAscii - 96;
+        return priority;
+    }
+    throw new ArgumentException("Char should be alphabetical");
+}
---
+++ Advent3/Part2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+var lines = File.ReadLines("input");
+
+int prioritySum = 0;
+
+for (int i = 0; i < lines.Count(); i += 3)
+{
+    StringBuilder sb = new StringBuilder();
+    for (int j = i; j < i + 3; j++)
+    {
+        // Remove duplicates in line, join lines together
+        sb.Append(new string(lines.ElementAt(j).Distinct().ToArray()));
+    }
+    String joined = sb.ToString();
+    Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+    foreach (char ch in joined)
+    {
+        if (charCount.ContainsKey(ch))
+        {
+            charCount[ch]++;
+        }
+        else
+        {
+            charCount[ch] = 1;
+        }
+    }
+
+    char maxKey = char.MinValue;
+    int maxValue = 0;
+    foreach (KeyValuePair<char, int> entry in charCount)
+    {
+        if (entry.Value > maxValue)
+        {
+            maxValue = entry.Value;
+            maxKey = entry.Key;
+        }
+    }
+
+    //Console.WriteLine("Max key = " + maxKey);
+    prioritySum += GetItemPriority(maxKey);
+}
+
+Console.WriteLine("Sum of priorities: " + prioritySum);
+
+int GetItemPriority(char item)
+{
+    int priority;
+    int itemAscii = (int)item;
+    if (itemAscii is >= 65 and <= 90)
+    {
+        //Uppercase letter
+        priority = itemAscii - 38;
+        return priority;
+    } else if (itemAscii is >= 97 and <= 122)
+    {
+        //Lowercase letter
+        priority = itemAscii - 96;
+        return priority;
+    }
+    throw new ArgumentException("Char should be alphabetical");
+}
