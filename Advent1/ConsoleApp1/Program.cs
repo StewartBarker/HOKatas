c103e751bdc9594b89633735ac429352eabeb72c
Matt Coulton <mattcoulton@192.168.1.120>
2023-03-08 11:33:30 +0000

    Day 1 solution

---
+++ Advent1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿var lines = File.ReadLines("input");
+
+List<int> elves= new List<int>();
+
+int val = 0;
+foreach (string line in lines)
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        elves.Add(val);
+        val = 0;
+        continue;
+    }
+    val += int.Parse(line);
+}
+
+// Catch final elf not ending in a line break
+if (val != 0)
+{
+    elves.Add(val);
+}
+
+int maxValue = elves.Max();
+Console.WriteLine(maxValue);
+
---
+++ Advent1/Part2/Program.cs
@@ -0,0 +1,30 @@
+﻿var lines = File.ReadLines("example");
+
+List<int> elves= new List<int>();
+
+int val = 0;
+foreach (string line in lines)
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        elves.Add(val);
+        val = 0;
+        continue;
+    }
+    val += int.Parse(line);
+}
+
+// Catch final elf not ending in a line break
+if (val != 0)
+{
+    elves.Add(val);
+}
+
+// Sum top three values
+int topThreeSum = 0;
+elves.Sort();
+for (int i = elves.Count-3; i < elves.Count ; i++)
+{
+    topThreeSum += elves[i];
+}
+Console.WriteLine(topThreeSum);
