1d30305751c59a0a87f2c3c909a139027f094f1b
Matt Coulton <mcoulton@live.co.uk>
2023-03-08 16:50:47 +0000

    Day 4

---
+++ Advent4/Part1/Program.cs
@@ -0,0 +1,23 @@
+﻿var lines = File.ReadAllLines("input");
+
+int containedCount = 0;
+
+foreach (String line in lines)
+{
+    string[] elves = line.Split(",");
+    int[] range1 = Array.ConvertAll(elves[0].Split("-"), int.Parse);
+    int[] range2 = Array.ConvertAll(elves[1].Split("-"), int.Parse);
+    if (Contains(range1, range2) || Contains(range2, range1))
+    {
+        containedCount++;
+    }
+}
+
+Console.WriteLine("Number of pairs where one range fully encompasses the other: " + containedCount);
+
+
+
+bool Contains(int[] inner, int[] outer)
+{
+    return inner[0] >= outer[0] && inner[1] <= outer[1];
+}
---
+++ Advent4/Part2/Program.cs
@@ -0,0 +1,27 @@
+﻿var lines = File.ReadAllLines("input");
+
+int overlapsCount = 0;
+
+foreach (String line in lines)
+{
+    string[] elves = line.Split(",");
+    int[] range1 = Array.ConvertAll(elves[0].Split("-"), int.Parse);
+    int[] range2 = Array.ConvertAll(elves[1].Split("-"), int.Parse);
+    if (Overlaps(range1, range2))
+    {
+        overlapsCount++;
+    }
+}
+
+Console.WriteLine("Number of pairs where the two ranges overlap: " + overlapsCount);
+
+
+bool Overlaps(int[] range1, int[] range2)
+{
+    // Cannot overlap if one range ends before other's start or starts after other's end
+    if (range1[1] < range2[0] || range1[0] > range2[1])
+    {
+        return false;
+    }
+    return true;
+}
