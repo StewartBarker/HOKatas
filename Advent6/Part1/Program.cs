3b5cce86c6422709b8ba2dcb4b5b557d8fd92a8d
Matt Coulton <mcoulton@live.co.uk>
2023-03-09 15:18:09 +0000

    Day 6

---
+++ Advent6/Part1/Program.cs
@@ -0,0 +1,17 @@
+﻿const int markerLength = 4;
+
+String input = File.ReadAllLines("input")[0];
+
+for (int i = 0; i <= input.Length-markerLength; i++)
+{
+    if (IsMarker(input.Substring(i, markerLength)))
+    {
+        Console.WriteLine("Answer: " + (i+markerLength));
+        return;
+    }
+}
+
+bool IsMarker(String range)
+{
+    return range.Length == range.Distinct().Count();
+}
